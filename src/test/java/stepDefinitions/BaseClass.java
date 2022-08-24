a6b0146de002e62c02c46d75bffad36e655dded7
Yogita Sharma <sharmayogita166@gmail.com>
2022-08-23 17:47:12 -0700

    push framework code

---
+++ src/test/java/stepDefinitions/BaseClass.java
@@ -0,0 +1,57 @@
+/*-----Created By Yogita--------*/
+package stepDefinitions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static Properties prop;
+	
+
+	public BaseClass() {
+		try {
+			prop = new Properties();
+			FileInputStream fi = new FileInputStream(System.getProperty("user.dir") + "\\config.properties");
+			prop.load(fi);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void launchApp() {
+		String browserName = prop.getProperty("browserff");
+		if (browserName.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", prop.getProperty("chromepath"));
+			driver = new ChromeDriver();
+
+		} else if (browserName.equalsIgnoreCase("FireFox")) {
+			System.setProperty("webdriver.gecko.driver", prop.getProperty("firefoxpath"));
+			driver = new FirefoxDriver();
+
+		} else if (browserName.equalsIgnoreCase("IE")) {
+			System.setProperty("webdriver.ie.driver", prop.getProperty("IEpath"));
+			driver = new InternetExplorerDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Integer.parseInt(prop.getProperty("implicitWait")), TimeUnit.SECONDS);
+		// PageLoad TimeOuts
+		driver.manage().timeouts().pageLoadTimeout(Integer.parseInt(prop.getProperty("pageLoadTimeOut")),
+				TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+	}
+}
---
+++ src/test/java/testRunner/TestRun.java
@@ -0,0 +1,22 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"Features/"},
+		glue={"stepDefinitions"},
+		dryRun=false,
+		monochrome = true, //display the console output in a proper readable format
+		strict=true // it will check if any step is not defined in step defination file
+		//plugin = {"pretty","html=test-output"}
+		)
+
+public class TestRun {
+	
+}
